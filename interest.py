db78292dfa6097ddc6b3455e78dbe685988d48b2
arielboutcher <arielos11556@gmail.com>
2019-05-26 19:26:54 +0300

    code for simple interest

---
+++ interest.py
@@ -0,0 +1,17 @@
+#calculates simple interest
+#formula>>>> simpleinterest = principal*rate*time
+
+#we are going to let the rate be constant
+rate = 0.12
+
+#the principal is going to be input by the banker
+principal = int(input("Enter the principal amount"))
+
+#the time is going to be input by the banker
+time = int(input("Enter the time in years"))
+
+#we now calculate the simple interest
+interest = principal*rate*time
+
+#we now output the simple interest and use string formatting to insert it into a string
+print("The simple interest is: {}".format(interest))
